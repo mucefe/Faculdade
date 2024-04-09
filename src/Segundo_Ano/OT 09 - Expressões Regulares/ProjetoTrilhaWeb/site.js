f09e9331e172936dfa14892a20578c515dc60e7c
mucefe <mucefe@yahoo.com.br>
2024-04-08 21:05:25 -0300

    Disponibilizando a primeira parte do exercício da OT 09
    RegExp em JavaScript

--- src/Segundo_Ano/OT 09 - Expressões Regulares/ProjetoTrilhaWeb/site.js
+++ src/Segundo_Ano/OT 09 - Expressões Regulares/ProjetoTrilhaWeb/site.js
@@ -54,8 +54,32 @@ function verificaMotivo(motivo) {
         if (elemento.firstChild)
             elemento.removeChild(elemento.firstChild);
         
-        }
-     
-       
-    
+        }   
+}
+ function verificaNome(){
+var nome = document.frmfaleconosco.txtnome.value;           // Recebe o valor do campo txtnome em uma variável
+
+
+var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");  // Cria a expressão regular como um objeto de 
+                                                                   // ExpReg e o atribui a expRegNome
+if (!expRegNome.test(nome)){                              // Testar o nome digitado com a expressão regular, onde se o resultado for
+                                                          // falso (devido ao caractere “!”, que assim como no Java, representa
+                                                        // negação), mostra o alerta, foca no campo e retorna falso.
+    alert("Preencha o campo Nome corretamente");
+    document.frmfaleconosco.txtnome.focus();
+    return false;
+}
+}
+
+function verificatelefone(){
+var fone = document.frmfaleconosco.txtfone.value;
+var expRegFone = new RegExp ("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4,5}$");
+
+if (!expRegFone.test(fone)){
+    alert("Preencha o campo Telefone corretamente.");
+document.frmfaleconosco.txtfone.focus();
+return false;
+}
+
+
 }
