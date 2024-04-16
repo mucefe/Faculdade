86ee31a7503b4acbe64f7ba6f9755eeb5e1b994a
mucefe <mucefe@yahoo.com.br>
2024-04-15 21:25:02 -0300

    Disponibilizando os arquivos da OT 01 da trilha de BackEnd

---
+++ src/Segundo_Ano/TrilhaBackEnd/OT 01 - Introducao ao Node.JS/CalculadoraNode/calc.js
@@ -0,0 +1,8 @@
+let SomaFunc = require("./somar");
+let DivisaoFunc = require("./dividir");
+let SubtracaoFunc = require("./subtrair")
+let MultiplicacaoFunc = require("./multiplicar");
+console.log(`O valor da soma é: ${SomaFunc(1,2)}`);
+console.log(`O valor da divisao é: ${DivisaoFunc(15,3)}`);
+console.log(`O valor da multiplicação é: ${MultiplicacaoFunc(5,4)}`);
+console.log(`O valor da subtração é: ${SubtracaoFunc(15,7)}`);
