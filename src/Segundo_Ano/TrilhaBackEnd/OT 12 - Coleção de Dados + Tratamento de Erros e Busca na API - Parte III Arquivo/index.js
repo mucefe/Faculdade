09d032fa8501a2fa3c79edc6d8a5adab7a01a4a4
mucefe <mucefe@yahoo.com.br>
2024-05-20 20:33:56 -0300

    Atualizando os arquivos da OT 12 da trilha de Node.JS
    Coleção de Dados + Tratamento de Erros e Busca na API - Parte III Arquivo

---
+++ src/Segundo_Ano/TrilhaBackEnd/OT 12 - Coleção de Dados + Tratamento de Erros e Busca na API - Parte III Arquivo/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import colecaoUf from './dados.js';
+
+const app = express();
+
+app.get('/ufs', (req, res) => {
+    res.json(colecaoUf);
+});
+
+app.get('/ufs/:iduf', (req, res) => {
+    const iduf = parseInt(req.params.iduf);  
+    let uf = null;
+    let mensagemErro = '';
+
+    if (!isNaN(iduf)) {
+        uf = colecaoUf.find(u => u.id === iduf);
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida';
+    }   
+    
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).send({ "erro": mensagemErro });
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080');
+});
