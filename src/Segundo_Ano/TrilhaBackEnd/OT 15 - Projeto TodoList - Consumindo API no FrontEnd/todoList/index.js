5d856100d9c631228f3bebf851f1ba7fd71d00f2
mucefe <mucefe@yahoo.com.br>
2024-05-29 20:42:35 -0300

    Otimização do código
    Node.JS

--- src/Segundo_Ano/TrilhaBackEnd/OT 15 - Projeto TodoList - Consumindo API no FrontEnd/todoList/index.js
+++ src/Segundo_Ano/TrilhaBackEnd/OT 15 - Projeto TodoList - Consumindo API no FrontEnd/todoList/index.js
@@ -8,8 +8,8 @@ app.set('view engine', 'html');
 app.use('/public',express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use( bodyParser.json() );       
+app.use(bodyParser.urlencoded({     
   extended: true
 })); 
 
@@ -19,19 +19,19 @@ app.get('/', (req,res)=>{
     res.render('index',{tasksList: tasks});
 });
 
+app.get('/deletar/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    if (id >= 0 && id < tasks.length) {
+        tasks.splice(id, 1); 
+    }
+    res.redirect('/'); 
+});
+
 app.post('/',(req,res)=>{
     tasks.push(req.body.task);
     res.render('index',{tasksList: tasks});
 })
 
-
-app.get('/deletar/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    if (id >= 0 && id < tasks.length) {
-        tasks.splice(id, 1); // Remove o item do array na posição do ID
-    }
-    res.redirect('/'); // Redireciona de volta para a rota principal '/'
-});
 
 app.listen(5000,()=>{
     console.log('Servidor rodando em http://localhost:5000');
