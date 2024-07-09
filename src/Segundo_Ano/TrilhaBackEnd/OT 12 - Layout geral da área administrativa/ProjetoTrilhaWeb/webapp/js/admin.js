bfa41a13895a929b447ae966069a871ce6e1d431
mucefe <mucefe@yahoo.com.br>
2024-07-09 20:08:03 -0300

    Subindo os arquivos das OTs EaD 06 (boootstrap) e 12 (BackEnd)
    Projeto Afoxinhas e Layout geral da área administrativa do projeto Coldigo Geladeiras.

---
+++ src/Segundo_Ano/TrilhaBackEnd/OT 12 - Layout geral da área administrativa/ProjetoTrilhaWeb/webapp/js/admin.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+    $("header").load("exemploServlet/src/main/webapp/pages/admin/general/header.html");
+    $("footer").load("exemploServlet/src/main/webapp/pages/admin/general/footer.html");
+});
