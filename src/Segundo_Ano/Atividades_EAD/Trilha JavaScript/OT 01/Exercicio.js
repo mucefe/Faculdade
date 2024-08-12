5c46d81f967abf34e94671759ec6097ebd06225c
mucefe <mucefe@yahoo.com.br>
2024-08-12 16:32:14 -0300

    Adicionando OT 01 da trilha de Javascript e OT 12 da trilha de REST
    O primeiro exercício da nova trilha de Javascript

---
+++ src/Segundo_Ano/Atividades_EAD/Trilha JavaScript/OT 01/Exercicio.js
@@ -0,0 +1,13 @@
+// 1. Crie 3 variáveis contendo:
+// ● Seu nome;
+// ● Sua idade;
+// ● Comida favorita;
+// Após declarar as variáveis e atribuir os valores, utilize o console.log para
+// formar uma frase como:
+// “Meu nome é …, tenho …. anos e minha comida favorita é …..”
+
+let nome = "Murilo";
+let idade = 36;
+let comidaFavorita = "Espinafre"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e minha comida favorita é ${comidaFavorita}`);
