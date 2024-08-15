9d3756b1fd272c7520c7abd059d3c89a291e340d
mucefe <mucefe@yahoo.com.br>
2024-08-15 20:20:29 -0300

    Atualizações no projeto Coldigo Geladeiras
    As OTS 12, 13,14 e 15 estão todas na pasta da OT 12, para evitar ter de ficar copiando o projeto Coldigo, o que tem gerado muitos erros com o Apache Tomcat e com o Eclipse.

--- src/Segundo_Ano/TrilhaBackEnd/OT 12 - Layout geral da área administrativa/ProjetoTrilhaWeb/src/main/webapp/js/admin.js
+++ src/Segundo_Ano/TrilhaBackEnd/OT 12 - Layout geral da área administrativa/ProjetoTrilhaWeb/src/main/webapp/js/admin.js
@@ -1,5 +1,19 @@
+COLDIGO = new Object();
+
 $(document).ready(function() {
+	
 	$("header").load("/ProjetoTrilhaWeb/pages/admin/general/header.html");
 	$("footer").load("/ProjetoTrilhaWeb/pages/admin/general/footer.html");
-	console.log("admin.js rodando.");
+	
+	
+	COLDIGO.carregaPagina = function(pagename){
+		$("section").empty();
+		$("section").load(pagename + "/", function(response, status, info) {
+			if (status == "error"){
+				var msg = "Houve um erro ao encontrar a página: "+ info.status + " - " + info.statusText;
+				$("section").html(msg);
+			}			
+		})
+	}
+	
 });
