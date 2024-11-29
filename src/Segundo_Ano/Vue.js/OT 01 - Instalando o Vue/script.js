ab8d297b0f4410b115ff51e9b109d89e3ab5e0f1
mucefe <mucefe@yahoo.com.br>
2024-11-29 20:08:39 -0300

    Disponibilizando as OTs 01,02 e 03 da trilha de Vue.JS
    Introdução ao Vue, Diretivas e atividades

---
+++ src/Segundo_Ano/Vue.js/OT 01 - Instalando o Vue/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btenviar').addEventListener("click", function() {
+   var mensagem = getElementById("#mensagem");
+   // Se a visibilidade do elemento #mensagem estiver como 'invisible', ele muda para 'visible' e vice-versa.
+});
